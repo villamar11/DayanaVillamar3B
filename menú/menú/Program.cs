67b9c281e3471062178d6022185bf7040064153e
villamar11 <56278168+villamar11@users.noreply.github.com>
2019-10-16 19:36:05 -0500

    Add files via upload

---
+++ menú/menú/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace menú
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int opción;
+            do
+            {
+                int num1, num2, numero, factorial, suma = 0;
+                Console.WriteLine("**********MENÚ**********");
+                Console.WriteLine("\nEscoja una opción del menú:");
+                Console.WriteLine();
+                Console.WriteLine("1-> Salir");
+                Console.WriteLine();
+                Console.WriteLine("2-> Sumatorio");
+                Console.WriteLine();
+                Console.WriteLine("3-> Factorial");
+                Console.WriteLine();
+                Console.WriteLine("*************************");
+                opción = Convert.ToInt16(Console.ReadLine());
+
+                switch (opción)
+                {
+                    case 1:
+                        Console.WriteLine("usted acaba de salir del programa");
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.WriteLine("ingrese los valores para su suma");
+                        num1 = Convert.ToInt16(Console.ReadLine());
+                        num2 = Convert.ToInt16(Console.ReadLine());
+                        suma = num1 + num2;
+                        Console.WriteLine("la suma de sus números ingresados es -->" + suma);
+                        Console.WriteLine();
+                        Console.WriteLine("Ingresar cualquier tecla para continuar:");
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        Console.WriteLine("Ingrese un número para calcular el factorial:");
+                        numero = Convert.ToInt16(Console.ReadLine());
+                        factorial = numero;
+                        for (int r = numero - 1; r >= 1; r--)
+                        {
+                            factorial = factorial * r;
+                        }
+                        Console.WriteLine("el factorial de su nmero ingresado es-->" + factorial);
+                        Console.WriteLine();
+                        Console.WriteLine("Ingresar cualqueir tecla para continuar:");
+                        Console.ReadKey();
+                        break;
+
+                }
+
+            } while (opción != 1);
+
+
+        }
+    }
+}
---
+++ piramideasteriscos/piramideasteriscos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace piramideasteriscos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese un número el cual será el tamaño de su piramide");
+            int tamaño = Convert.ToInt16(Console.ReadLine());
+
+            for (int i = 1; i <= tamaño; i++)
+            {
+                for (int j = 1; j <= tamaño - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int g = 1; g <= (2 * i) - 1; g++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
